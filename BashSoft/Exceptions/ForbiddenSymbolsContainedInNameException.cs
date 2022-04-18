f8f2292bfb4532181104e8138e7b01050ecbdbc2
Petko Pedev <petko.pedev@gmail.com>
2022-04-18 20:21:47 +0300

    Add few other exceptions

---
+++ BashSoft/Exceptions/ForbiddenSymbolsContainedInNameException.cs
@@ -0,0 +1,17 @@
+﻿namespace BashSoft.Exceptions
+{
+    public class ForbiddenSymbolsContainedInNameException : Exception
+    {
+        private const string ForbiddenSymbolsContainedInName = "The given name contains symbols that are not allowed to be used in names of files and folders.";
+
+        public ForbiddenSymbolsContainedInNameException() : base(ForbiddenSymbolsContainedInName)
+        {
+
+        }
+
+        public ForbiddenSymbolsContainedInNameException(string message) : base(message)
+        {
+
+        }
+    }
+}
